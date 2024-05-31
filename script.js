fa777124b8682bce11dd26b9ce1197989a04e070
Rahul <rahul@vrittechnologies.com>
2024-05-31 12:33:10 +0545

    card first comment

---
+++ script.js
@@ -0,0 +1,22 @@
+switch("thu")
+{ 
+    case "sun" : console.log("sunday"); 
+    break; 
+
+    case "mon" : console.log("monday"); 
+    break; 
+
+    case "tue" : console.log("tuesday"); 
+    break; 
+
+    case "wed" : console.log("wednesday"); 
+    break; 
+
+    case "thu" : console.log("thusday"); 
+    break; 
+
+    case "fri" : console.log("friday");
+    break; 
+
+    default: console.log("saturday") 
+}
